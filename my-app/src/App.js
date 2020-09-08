6ef7525758f8574af8daba51cd517e972704fb6b
Amatullah Mir <amatullahmir@Amatullahs-MacBook-Pro.local>
2020-09-08 14:57:16 -0500

    react app programming

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My first React app!</h1>
+      <p>Welcome :)</p>
+    </div>
+  );
+}
+
+export default App;
